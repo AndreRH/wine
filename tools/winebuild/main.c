b671c74f7be6c3e56fb3be24a43ca10addaa8c19
André Hentschel <nerv@dawncrow.de>
2020-08-15 12:40:39 +0200

    winebuild: Add PPC64 support

--- tools/winebuild/main.c
+++ tools/winebuild/main.c
@@ -54,6 +54,8 @@ int safe_seh = 0;
 enum target_cpu target_cpu = CPU_x86;
 #elif defined(__x86_64__)
 enum target_cpu target_cpu = CPU_x86_64;
+#elif defined(__powerpc64__)
+enum target_cpu target_cpu = CPU_POWERPC64;
 #elif defined(__powerpc__)
 enum target_cpu target_cpu = CPU_POWERPC;
 #elif defined(__arm__)
