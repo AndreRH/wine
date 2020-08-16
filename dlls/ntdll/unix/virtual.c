a775a62796b1825c200b9ef1312a7a1f6ee7be42
André Hentschel <nerv@dawncrow.de>
2020-08-16 20:15:50 +0200

    ntdll: Add PPC64 signal handling

--- dlls/ntdll/unix/virtual.c
+++ dlls/ntdll/unix/virtual.c
@@ -2456,6 +2456,8 @@ NTSTATUS virtual_map_ntdll( int fd, void **module )
         nt.FileHeader.Machine != IMAGE_FILE_MACHINE_ARMNT) return STATUS_INVALID_IMAGE_FORMAT;
 #elif defined(__aarch64__)
     if (nt.FileHeader.Machine != IMAGE_FILE_MACHINE_ARM64) return STATUS_INVALID_IMAGE_FORMAT;
+#elif defined(__powerpc64__)
+    if (nt.FileHeader.Machine != IMAGE_FILE_MACHINE_POWERPC64) return STATUS_INVALID_IMAGE_FORMAT;
 #endif
 
     base  = (void *)nt.OptionalHeader.ImageBase;
