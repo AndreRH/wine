b2e761047f858f96f6bac2ecfedbcd3a0cb5e230
Billy Laws <blaws05@gmail.com>
2023-07-11 12:05:06 +0100

    ntdll: Perform initial WoW64 initialization after locales are set-up
    This allows the WoW64 BT module to use functions from kernel32, which implicitly
    depends on locales having been setup to initialise correctly.

--- dlls/ntdll/loader.c
+++ dlls/ntdll/loader.c
@@ -4311,8 +4311,6 @@ void loader_init( CONTEXT *context, void **entry )
         load_global_options();
         version_init();
 
-        if (NtCurrentTeb()->WowTebOffset) init_wow64( context );
-
         wm = build_main_module();
         build_ntdll_module();
 
@@ -4327,6 +4325,9 @@ void loader_init( CONTEXT *context, void **entry )
 
         actctx_init();
         locale_init();
+
+        if (NtCurrentTeb()->WowTebOffset) init_wow64( context );
+
         get_env_var( L"WINESYSTEMDLLPATH", 0, &system_dll_path );
         if (wm->ldr.Flags & LDR_COR_ILONLY)
             status = fixup_imports_ilonly( wm, NULL, entry );
