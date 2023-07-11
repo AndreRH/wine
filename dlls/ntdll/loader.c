3d42331aa51e58a671168e0b777cbdfd31eadff6
Billy Laws <blaws05@gmail.com>
2023-07-11 12:09:48 +0100

    ntdll: Perform per-thread WoW64 initialization after TLS has been initialized
    This allows the WoW64 BT module to use implicit TLS, as otherwise when running
    BTCpuSimulate the module would end up referencing TLS memory that hasn't yet
    been allocated.

--- dlls/ntdll/loader.c
+++ dlls/ntdll/loader.c
@@ -4400,9 +4400,6 @@ void loader_init( CONTEXT *context, void **entry )
     }
     else
     {
-#ifdef _WIN64
-        if (NtCurrentTeb()->WowTebOffset) init_wow64( context );
-#endif
 #ifdef __arm64ec__
         arm64ec_thread_init();
 #endif
@@ -4455,6 +4452,10 @@ void loader_init( CONTEXT *context, void **entry )
         thread_attach();
         if (wm->ldr.TlsIndex == -1) call_tls_callbacks( wm->ldr.DllBase, DLL_THREAD_ATTACH );
     }
+
+#ifdef _WIN64
+    if (NtCurrentTeb()->WowTebOffset) init_wow64( context );
+#endif
 
     RtlLeaveCriticalSection( &loader_section );
 }
