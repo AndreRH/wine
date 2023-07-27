8fc4c9bbc7023565b6bcec6097e76cb421c3a237
Billy Laws <blaws05@gmail.com>
2023-07-27 08:00:43 -0700

    ntdll: Implement WOW64 thread suspension helpers

--- dlls/ntdll/loader.c
+++ dlls/ntdll/loader.c
@@ -4057,6 +4057,7 @@ static void load_global_options(void)
 static void (WINAPI *pWow64LdrpInitialize)( CONTEXT *ctx );
 
 void (WINAPI *pWow64PrepareForException)( EXCEPTION_RECORD *rec, CONTEXT *context ) = NULL;
+NTSTATUS (WINAPI *pWow64SuspendLocalThread)( HANDLE thread, ULONG *count ) = NULL;
 
 static void init_wow64( CONTEXT *context )
 {
@@ -4078,6 +4079,7 @@ static void init_wow64( CONTEXT *context )
 
         GET_PTR( Wow64LdrpInitialize );
         GET_PTR( Wow64PrepareForException );
+        GET_PTR( Wow64SuspendLocalThread );
 #undef GET_PTR
         imports_fixup_done = TRUE;
     }
